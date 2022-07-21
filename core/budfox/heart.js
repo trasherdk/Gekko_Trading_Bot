77f6cbdd015e078a3eba48f4c980630715ff2c46
TUDTech <81108192+TUDTech@users.noreply.github.com>
2022-07-21 14:14:41 -0500

    Update heart.js

--- core/budfox/heart.js
+++ core/budfox/heart.js
@@ -29,9 +29,8 @@ Heart.prototype.pump = function() {
 Heart.prototype.tick = function() {
   if(this.lastTick) {
     // make sure the last tick happened not to lang ago
-    // @link https://github.com/askmike/gekko/issues/514
     if(this.lastTick < moment().unix() - TICKRATE * 3)
-      util.die('Failed to tick in time, see https://github.com/askmike/gekko/issues/514 for details', true);
+      util.die('Failed to tick in time', true);
   }
 
   this.lastTick = moment().unix();
